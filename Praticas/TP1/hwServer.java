c266f618b659e671d2025517fa285dfaedc918fe
Pedro <up201809694@fe.up.pt>
2021-11-02 23:40:04 +0000

    Add TP1

---
+++ Praticas/TP1/hwServer.java
@@ -0,0 +1,31 @@
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+//
+//  Hello World server in Java
+//  Binds REP socket to tcp://*:5555
+//  Expects "Hello" from client, replies with "World"
+//
+
+public class hwServer {
+    public static void main(String[] args) throws Exception {
+        try (ZContext zc = new ZContext) {
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(SocketType.REP);
+            socket.bind("tcp://*:5555");
+
+            while (!Thread.currentThread().isInterrupted()) {
+                byte[] reply = socket.recv(0);
+                System.out.println(
+                    "Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]"
+                );
+
+                Thread.sleep(1000);
+
+                String response = "world";
+                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+            }
+        }
+    }
+}
